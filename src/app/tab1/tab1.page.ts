d85c3dc4160fc291ab0fe84e3afc5e9b929d7e73
Hizir Cagin Bektas <hizircaginbektas@Hizirs-MacBook-Air.local>
2020-11-15 17:54:28 +0100

    leaflet init

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import * as L from "leaflet";
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon-2x.png";
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  map: L.Map
+
   constructor() {}
+
+  ngOnInit() {
+    this.map = L.map('map', {
+      center: [ -42.737669, 147.436224 ],
+      zoom: 60,
+      renderer: L.canvas()
+    })
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map)
+
+
+    setTimeout(() => {
+      this.map.invalidateSize();
+    }, 0);
+
+    var marker = L.marker([ -42.737669, 147.436224]).addTo(this.map);
+
+  }
 
 }
