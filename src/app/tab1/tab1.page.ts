d7b0810562cbc041e8ea0406155add526f7c4b69
Hizir Cagin Bektas <hizircaginbektas@Hizirs-MacBook-Air.local>
2020-11-16 13:20:26 +0100

    initial and further locaiton updates added.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 import * as L from "leaflet";
 import "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/images/marker-icon-2x.png";
@@ -12,14 +13,32 @@ import "leaflet/dist/images/marker-icon-2x.png";
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
   map: L.Map
+  lat: any =50.041518
+  long: any =14.555797
+  marker: any
 
-  constructor(private geolocation: Geolocation, private sucukluTost: ToastController) {}
+  constructor(private geolocation: Geolocation, private sucukluTost: ToastController, private alertController: AlertController) {}
+
+  ngOnInit() {
+    //this.getInitialLocation();
+    this.generateMap()
+  }
+
+  getInitialLocation(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = resp.coords.latitude;
+      this.long = resp.coords.longitude;
+      this.presentToast("Location updated", 1000);
+      this.generateMap();
+     }).catch((error) => {
+       this.presentToast("Error getting location", 2000);
+     });
+  }
 
   generateMap(){
     this.map = L.map('map', {
-      center: [ -42.737669, 147.436224 ],
+      center: [ this.lat, this.long ],
       zoom: 60,
       renderer: L.canvas()
     })
@@ -34,34 +53,75 @@ export class Tab1Page {
     }, 0);
 
     var marker = L.marker([ -42.737669, 147.436224]).addTo(this.map);
+
+    this.createMarker(this.lat, this.long)
   }
 
-  ngOnInit() {
-    this.generateMap();
-  }
-
-  populateCurrentLocation(){
-    this.map.flyTo(new L.LatLng(40.987579, 29.036931), 40, {
-      animate: false
-    });
-  }
-  getCurrentLocatin(){
+  updateCurrentLocation(){
     this.geolocation.getCurrentPosition().then((resp) => {
-      var marker = L.marker([ resp.coords.latitude, resp.coords.longitude]).addTo(this.map);
-
-      this.map.flyTo(new L.LatLng(resp.coords.latitude, resp.coords.longitude), 30, {
-        animate: false
-      });
+      this.lat = resp.coords.latitude;
+      this.long = resp.coords.longitude;
+      this.presentToast("Location updated", 1000);
+      this.updateMap();
      }).catch((error) => {
-       console.log('Error getting location');
        this.presentToast("Error getting location", 2000);
      });
   }
+
+  updateMap(){
+    this.map.flyTo(new L.LatLng(this.lat, this.long), 30, {
+      animate: false
+    });
+    this.createMarker(this.lat, this.long);
+  }
+
+  createMarker(lat: any, long: any){
+    if(this.marker && this.marker != undefined)
+      this.deleteMarker();
+     this.marker = L.marker([ lat, long]);
+     this.marker.addTo(this.map);
+  }
+
+  deleteMarker(){
+    this.map.removeLayer(this.marker)
+  }
+
   async presentToast(message: string, duration: number) {
     const toast = await this.sucukluTost.create({
       message: message,
       duration: 3000
     });
     toast.present();
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Select an option',
+      buttons: [
+        {
+          text: 'Improve location',
+          handler: () => {
+            this.presentToast("Location improving...", 1000);
+            this.updateCurrentLocation();
+          }
+        }, 
+        {
+          text: 'Navigate to last parking',
+          handler: () => {
+            this.presentToast("WIP...", 1000);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'danger',
+          handler: (blah) => {
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
